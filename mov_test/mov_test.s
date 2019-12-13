7bdfd1dd5b809593b5dc725167f8cc5a7ab0f243
Alejandro Druetta <aledruetta@gmail.com>
2019-12-13 15:47:36 -0300

    update move test

--- mov_test/mov_test.s
+++ mov_test/mov_test.s
@@ -1,4 +1,4 @@
-.global _start
+	.global _start
 
 _start:
 
@@ -6,6 +6,18 @@ _start:
 	movt	r2, #0x4f5d	@ movimenta a constante para 2 bytes mais significativos
 				@ o resultado vai ser 4f5d6e3a
 
+	mov	r3, r2		@ movimenta o conteudo de r2 para r3
+
+	mov	r2, #8
+	mov	r3, r2, LSL #1	@ Logical shift left
+	mov	r3, r2, LSR #1	@ Logical shift right
+	mov	r3, r2, ASR #1	@ Arithmetic shift right
+	mov	r3, r2, ROR #1	@ Rotate right
+	mov	r3, r2, RRX	@ Rotate extended right
+
+	mvn	r3, #45		@ Move and NOT
+
+	mov	r0, #0
 	mov	r7, #1
 	svc 0
 
