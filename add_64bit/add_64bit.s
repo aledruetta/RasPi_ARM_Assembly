3228b268ecfecffac89be9d1740b773c427e0b9e
Alejandro Druetta <aledruetta@gmail.com>
2019-12-16 13:57:44 -0300

    adds and adc

--- add_64bit/add_64bit.s
+++ add_64bit/add_64bit.s
@@ -6,8 +6,8 @@ _start:
 	mov	r4, #0x00000005
 	mov	r5, #0x00000001
 
-	adds	r1, r3, r5
-	adc	r0, r2, r4
+	adds	r1, r3, r5	@ add with status flags
+	adc	r0, r2, r4	@ add with carry
 
 	mov	r7, #1
 	svc	0
