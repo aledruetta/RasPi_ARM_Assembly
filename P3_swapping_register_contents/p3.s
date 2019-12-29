e21833b004e6b646b2dbf5012a312651225df3b8
Alejandro Druetta <aledruetta@gmail.com>
2019-12-29 09:56:28 -0300

    swapping register contents

---
+++ P3_swapping_register_contents/p3.s
@@ -0,0 +1,14 @@
+@ Swap register contents
+
+	.global _start
+
+_start:
+
+	ldr	r0, =0xf631024c		@ load
+	ldr	r1, =0x17539abd
+	eor	r0, r0, r1		@ exclusive or
+	eor	r1, r0, r1
+	eor	r0, r0, r1
+
+	mov	r7, #1
+	svc	0
