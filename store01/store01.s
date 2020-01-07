0812008ad7df6e572bdd2b296a31d708303f9e6e
Alejandro Druetta <aledruetta@gmail.com>
2020-01-07 11:45:02 -0300

    comments alternatives store01

--- store01/store01.s
+++ store01/store01.s
@@ -3,6 +3,7 @@
 	.data
 
 	.align	4
+S:	.asciz	"string"
 myvar1: .word	0
 
 	.align	4
@@ -14,15 +15,17 @@ myvar2: .word	0
 
 main:
 	ldr	r1, addr_of_myvar1
+	@ ldr	r1, =myvar1
 	mov	r3, #3
 	str	r3, [r1]
 	ldr	r2, addr_of_myvar2
+	@ ldr	r2, =myvar2
 	mov	r3, #4
 	str	r3, [r2]
 
-	ldr	r1, addr_of_myvar1
+	@ ldr	r1, addr_of_myvar1
 	ldr	r1, [r1]
-	ldr	r2, addr_of_myvar2
+	@ ldr	r2, addr_of_myvar2
 	ldr	r2, [r2]
 	add	r0, r1, r2
 	bx	lr
