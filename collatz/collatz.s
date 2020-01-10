7e28ec4ce2a7bcf84c1bb09fd7d0edfbfba90b03
Alejandro Druetta <aledruetta@gmail.com>
2020-01-10 18:28:21 -0300

    fix collatz

--- collatz/collatz.s
+++ collatz/collatz.s
@@ -4,24 +4,27 @@
 .text
 
 main:
-	mov	r3, #22		@ n <- 22
+	mov	r3, #23		@ n <- some number
+	mov	r0, #0		@ number of steps
 
 loop:
 	cmp	r3, #1
-	beq	end_loop	@ if r3 == 1: end_while
+	beq	end		@ if r3 == 1: end_while
 
 	and	r4, r3, #1	@ r4 <- mask LSB
 	cmp	r4, #0
 	bne	odd		@ if r4 != 0: else_odd
 	lsr	r3, #1		@ r3 <- r3 / 2
-	b	loop
+	b	end_loop
 
 odd:
 	add	r4, r3, #1	@ temp r4 <- r3
 	lsl	r3, #1		@ r3 <- r3 * 2
 	add	r3, r3, r4	@ r3 <- r3 + (r3 * 2)
-	b	loop
 
 end_loop:
-	mov r0, r3
+	add	r0, r0, #1	@ increment steps
+	b	loop
+
+end:
 	bx	lr
